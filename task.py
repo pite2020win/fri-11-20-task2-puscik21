44159850fbc8b819ec3ba36abd420f633a2a0ed6
grzegorz.puscizna <grzesiekpuscizna@gmail.com>
2020-10-30 12:43:23 +0100

    Some basic logic

--- task.py
+++ task.py
@@ -28,3 +28,65 @@
 #
 #Delete these comments before commit!
 #Good luck.
+
+
+import time as timer
+
+
+class Diary:
+    def __init__(self):
+        self.students = []
+        self.subjects_names = []
+
+    # def act(self, action, time):
+    #     if action == "keep_speed":
+    #         self.keep_speed(time)
+    #         timer.sleep(1)
+
+    # def print_stats(self):
+    #     print("(wheel angle= " + str(self.wheel_angle) + ", speed=" + str(self.speed) + ")")
+
+    def add_student_to_subject(self, student, subject):
+        pass
+
+    def print_all_students(self):
+        pass
+
+    def print_students_average_in_subject(self, subject):
+        pass
+
+    def print_students_attendance(self):
+        pass
+
+    def print_student_subject_average(self, student):
+        pass
+
+
+class Student:
+    def __init__(self, name, surname):
+        self.subjects = []
+        self.attendance = 0
+        self.name = name
+        self.surname = surname
+
+    def get_average(self):
+        pass
+
+    def get_attendance(self):
+        pass
+
+
+class Subject:
+    def __init__(self):
+        self.name = 0
+        self.student_scores = []
+
+    def add_score(self, score):
+        self.student_scores.append(score)
+
+    def get_scores_average(self):
+        return sum(self.student_scores) / len(self.student_scores)
+
+
+if __name__ == '__main__':
+    print('Hello')
