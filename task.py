1218150b67cf9b8dee0b1b0091efd044d0bf11d1
grzegorz.puscizna <grzesiekpuscizna@gmail.com>
2020-10-31 11:42:38 +0100

    Added methods of adding students to subjects

--- task.py
+++ task.py
@@ -9,6 +9,9 @@ class Diary:
         for subject in file.read().splitlines():
             self.add_subject(subject)
 
+    def add_subject(self, subject_name):
+        self.subjects_names.add(subject_name)
+
     def add_new_students_from_file(self, file_path):
         file = open(file_path, "r")
         full_names = [full_name.split(", ") for full_name in file.read().splitlines()]
@@ -19,17 +22,14 @@ class Diary:
         student = Student(name, surname)
         self.students.append(student)
 
-    def add_subject(self, subject):
-        self.subjects_names.add(subject)
-
     def add_all_students_to_subjects(self):
-        pass
+        for student in self.students:
+            for subject_name in self.subjects_names:
+                student.add_to_subject(subject_name)
 
-    def add_student_to_subject_by_name(self, name, surname, subject):
-        pass
-
-    def add_student_to_subject(self, student, subject):
-        pass
+    def add_student_to_subject_by_student_names(self, name, surname, subject_name):
+        student = next(filter(lambda s: s.name == name and s.surname == surname, self.students), None)
+        student.add_to_subject(subject_name)
 
     def print_all_students(self):
         print(self.students)
@@ -38,12 +38,15 @@ class Diary:
         print(self.subjects_names)
 
     def print_students_average_in_subject(self, subject):
+        # todo
         pass
 
     def print_students_attendance(self):
+        # todo
         pass
 
     def print_student_subject_average(self, student):
+        # todo
         pass
 
 
@@ -55,11 +58,15 @@ class Student:
         self.average = 0
         self.subjects = set()
 
+    def add_to_subject(self, subject_name):
+        self.subjects.add(Subject(subject_name))
+
     def get_average(self):
         # todo count average from all subjects averages
         pass
 
     def get_attendance(self):
+        # todo
         pass
 
     def __repr__(self):
@@ -67,8 +74,8 @@ class Student:
 
 
 class Subject:
-    def __init__(self):
-        self.name = 0
+    def __init__(self, name):
+        self.name = name
         self.student_scores = []
 
     def add_score(self, score):
@@ -77,14 +84,19 @@ class Subject:
     def get_scores_average(self):
         return sum(self.student_scores) / len(self.student_scores)
 
+    def __repr__(self):
+        return self.name
+
 
 if __name__ == '__main__':
     diary = Diary()
     diary.add_new_subjects_from_file("subjects.txt")
     diary.add_new_students_from_file("students.txt")
+    diary.add_all_students_to_subjects()
+
     diary.print_all_students()
     diary.print_all_subjects()
-    diary.add_student_to_subject_by_name("Jan", "Kowalski", "math")
+    diary.add_student_to_subject_by_student_names("Jan", "Kowalski", "math")
 
 
 # todo uzywac setow jesli chodzi o subjecty studenta i studentow w dzienniku
